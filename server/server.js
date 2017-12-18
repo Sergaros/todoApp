f8ce85fc10ea6e2a08235bc8fe0f9cb3b6378b02
Sergej <sergej.tertychnij@gmail.com>
2017-12-18 18:16:45 +0200

    get user/me and uth middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const authenticate = require('./middleware/athenticate');
 
 const {ObjectId} = require('mongodb');
 
@@ -108,6 +109,12 @@ app.post('/users', (req, res) => {
     .catch(e=>{
         res.status(400).send(e);
     });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res)=>{
+    res.send(req.user);
 });
 
 if(require.main === module){
